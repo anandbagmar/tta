b2b91b74e854387b1d06ed4ee77b6ff585df5e07
kaavyat <kaavyat@thoughtworks.com>
2012-11-09 11:31:02 +0530

    Kaavya/Khushal: Created a rails project

---
+++ tta/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tta::Application.config.secret_token = '68cd0e4dd876035d53fe53b08531a01ceb051ebf20d3a0e6591d6e61a287f8ff89b2f170e124132eda2b4a260497cd183ce68a658cbdedb74dc5a2f889886332'
