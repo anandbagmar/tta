e0c16b529bc1a0d5bee3f19f08e48e7003786143
Avinash Kumar <avinashk@thoughtworks.com>
2012-11-15 14:28:43 +0530

    Avinash:Refactored test and made it proper

--- spec/services/x_unit_parser_spec.rb
+++ spec/services/x_unit_parser_spec.rb
@@ -1,20 +1,29 @@
 require 'spec_helper'
 describe XUnitParser do
   describe "parse xml" do
-    it "parses xml and retrieves metadata" do
 
-      parser = XUnitParser.new
+    before(:all) do
+        @parser = XUnitParser.new
+    end
+
+    it "parses xml and retrieves metadata" do
 
       xml = <<-eos
               <?xml version='1.0' encoding='UTF-8'?>
-              <testsuite tests="4" name="controllers.AccountAdminControllerTest" hostname="avinash-PC" failures="0" time="2.141" errors="0"/>
+              <testsuite tests="4"
+                         name="controllers.AccountAdminControllerTest"
+                         hostname="avinash-PC"
+                         failures="0"
+                         time="2.141"
+                         errors="0"
+              />
       eos
 
       xml_data = double("xml_data")
-      JunitXmlDatum.stub(:new){xml_data}
+      JunitXmlDatum.should_receive(:new).and_return(xml_data)
       xml_data.should_receive(:save)
 
-      parser.parse_xml(xml)
+      @parser.parse_xml(xml)
     end
 
   end
