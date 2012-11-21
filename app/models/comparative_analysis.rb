8125b0a3f2b402e6dfd6755b2dabf7af56772349
kaavyat <kaavyat@thoughtworks.com>
2012-11-21 18:16:46 +0530

    Kaavya: Integrated the flot with database

--- app/models/comparative_analysis.rb
+++ app/models/comparative_analysis.rb
@@ -1,4 +1,9 @@
 class ComparativeAnalysis
   # To change this template use File | Settings | File Templates.
-
+  def self.getProjectData number_of_tests
+    @project_data = JunitXmlDatum.select("tests, failures").inject([]) {|result, record|
+      result << [record.tests.to_i, record.failures.to_i] if record.tests && record.failures
+      result
+    }
+  end
 end
