73aeb0038c3b81bce3a2b1b1536176c182e7eb77
kaavyat <kaavyat@thoughtworks.com>
2012-11-22 18:22:41 +0530

    Kaavya : Created Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,29 +12,33 @@ puts "Seed data goes here!!"
   #ProjectMetaDatum.delete_all
   #TestRecord.delete_all
   1.upto(2) do |i|
-    project = Project.create!(:name => "PROJECT #{i}",
+    project = Project.create(:name => "PROJECT #{i}",
                    :type_of_report => "Junit",
-                   :authorization_level => "ALL").save
+                   :authorization_level => "ALL")
+    project.save
   end
 
   1.upto(5) do |i|
-    project_metadata = ProjectMetaDatum.create(:os_name => "Mac OS",
+    project_metadata = ProjectMetadatum.create(:os_name => "Mac OS",
                                                :host_name => "Test Machine",
                                                :browser => "Firefox",
                                                :date_of_execution => Date.current ,
                                                :user_timezone => Time.zone)
 
     project_metadata.project = Project.find(1)
+    project_metadata.save
+
     end
 
     1.upto(5) do |i|
-    project_metadata = ProjectMetaDatum.create(:os_name => "Windows OS",
+    project_metadata = ProjectMetadatum.create(:os_name => "Windows OS",
                                                :host_name => "Test Machine",
                                                :browser => "Chrome",
                                                :date_of_execution => Date.current ,
                                                :user_timezone => Time.zone)
 
     project_metadata.project = Project.find(2)
+    project_metadata.save
   end
 
 1.upto(200) do |i|
@@ -43,8 +47,8 @@ puts "Seed data goes here!!"
                                     :number_of_failures => "#{i}" ,
                                     :number_of_errors => "#{i}" ,
                                     :time_taken => "10")
-    test_record.project = Project.find(1)
-    test_record.project_metadata = ProjectMetaDatum.find(1)
+    test_record.project_metadatum = ProjectMetadatum.find(1)
+    test_record.save
     puts "Seed data done!!"
     end
 
@@ -54,7 +58,7 @@ puts "Seed data goes here!!"
                                     :number_of_failures => "#{i-1}" ,
                                     :number_of_errors => "#{i+1}" ,
                                     :time_taken => "8")
-    test_record.project = Project.find(1)
-    test_record.project_metadata = ProjectMetaDatum.find(2)
+    test_record.project_metadatum = ProjectMetadatum.find(2)
+    test_record.save
     puts "Seed data done!!"
   end
