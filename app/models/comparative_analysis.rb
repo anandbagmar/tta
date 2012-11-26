c2e3d26c41aea2a1d731a378a977baf36359efae
Aasawaree <aasawaree@aasawaree-Latitude-E6520.(none)>
2012-11-26 16:21:11 +0530

    Aasawaree : Addition of js file for time

--- app/models/comparative_analysis.rb
+++ app/models/comparative_analysis.rb
@@ -1,3 +1,5 @@
+
+
 class ComparativeAnalysis
   def self.getProjectData1
     @project_data = TestRecord.select("number_of_tests, number_of_failures").where(project_metadatum_id: 1).inject([]) {|result, record|
@@ -5,6 +7,7 @@ class ComparativeAnalysis
       result
     }
   end
+
   def self.getProjectData2
     @project_data = TestRecord.select("number_of_tests, number_of_failures").where(project_metadatum_id: 2).inject([]) {|result, record|
       result << [record.number_of_tests.to_i, record.number_of_failures.to_i] if record.number_of_tests && record.number_of_failures
