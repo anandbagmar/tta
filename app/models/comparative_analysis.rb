f4d6fe01607b5b4e681283dc7e497a80503f91ad
Pooja <pooja.akshantal@gmail.com>
2012-12-07 15:08:41 +0530

    [Sonia/Pooja] : Refactored comparative_analysis.rb

--- app/models/comparative_analysis.rb
+++ app/models/comparative_analysis.rb
@@ -2,33 +2,18 @@
 
 class ComparativeAnalysis
 
+  def self.getPercentageOfPassingTests(sub_project_id, start_date="1900-01-01",end_date="2200-12-31")
 
-  
-
-  def self.getPercentageOfPassingTests(sub_project_id,start_date="1900-01-01",end_date="2200-12-31")
-
-
-    @percentage_of_passing_tests = TestMetadatum.where(:sub_project_id => sub_project_id,:date_of_execution => start_date..end_date).inject([]) {|result, record|
-        total_num_of_tests = 0
-        number_of_failures = 0
-        TestRecord.find_all_by_test_metadatum_id(record.id).each do |test|
-          total_num_of_tests += test.number_of_tests.to_i
-          number_of_failures += test.number_of_failures.to_i
-
-        end
-        result << [(record.date_of_execution.to_time.to_f * 1000), (total_num_of_tests.to_f - number_of_failures.to_f) / total_num_of_tests.to_f  * 100]
+    meta_data = SubProject.find(sub_project_id).test_metadatum.find_all_by_date_of_execution(start_date..end_date)
+    meta_data.inject([]){ |result, metadata_record|
+      total_num_of_tests = 0
+      number_of_failures = 0
+      metadata_record.test_records.each do |test_record|
+        total_num_of_tests += test_record.number_of_tests.to_i
+        number_of_failures += test_record.number_of_failures.to_i
+      end
+      result << [(metadata_record.date_of_execution.to_time.to_f * 1000), (total_num_of_tests.to_f - number_of_failures.to_f) / total_num_of_tests.to_f  * 100]
     }
-
-    @percentage_of_passing_tests
   end
-
-  def self.getSubProjectName(project_id)
-    project_id=project_id.to_i
-    @project_name = SubProject.where(:id => project_id).inject([]){|result,record|
-    result << record.name
-    }
-    @project_name
-  end
-
 
 end
