52868103c5531683f4cedd7b7f3557ccd1e0a5c6
pooja-salpekar <salpekar.pooja4@gmail.com>
2012-12-13 14:40:08 +0530

    Khushal/Pooja: fixed bug: if no unit/functional/integration test in db, its percent on graph returned as 0%

--- app/controllers/visualization_controller.rb
+++ app/controllers/visualization_controller.rb
@@ -19,10 +19,10 @@ class VisualizationController < ApplicationController
       p ("*")*100
       p "end time:"
       p Time.now
+      @no_of_unit_test = calculate_number_of_test(result_set2)
+      @no_of_functional_test = calculate_number_of_test(result_set1)
+      @no_of_Integration_test = calculate_number_of_test(result_set3)
 
-      @no_of_unit_test = result_set2[0][0]
-      @no_of_Integration_test = result_set3[0][0]
-      @no_of_functional_test = result_set1[0][0]
       @sub_project_name = SubProject.find(sub_project_id).name
       calculate_percentage_of_tests
       @duration_functional=calculate_duration_in_hours(result_set1)
@@ -34,8 +34,14 @@ class VisualizationController < ApplicationController
   end
 
   def calculate_duration_in_hours(result_set)
+    if(result_set != [])
+      p "time is not nil"
     time = "%0.4f" % (result_set[0][1].to_f/60000)
     return time
+    else
+      return 0
+    end
+
   end
 
   def calculate_percentage_of_tests
@@ -44,6 +50,15 @@ class VisualizationController < ApplicationController
     @percent_functional_test = "%0.2f" % ((@no_of_functional_test/@total) *100)
     @percent_integration_test = "%0.2f" % ((@no_of_Integration_test/@total) *100)
 
+  end
+
+  def calculate_number_of_test(result_set)
+    if(result_set != [])
+      p "set is not nil"
+      return result_set[0][0]
+    else
+      return 0
+    end
   end
 
 end
