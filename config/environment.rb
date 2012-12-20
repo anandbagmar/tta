0642166b409e681c1cba3d025d69bec4500a7cb3
Anand Bagmar <abagmar@gmail.com>
2012-12-20 17:44:48 +0530

    Anand: Set the  in environment.rb which can be referenced from other places in the code

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Tta::Application.initialize!
+
+# ROOT FOLDER
+$PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), ".."))
+puts "$PROJECT_ROOT = #{$PROJECT_ROOT}"
+
