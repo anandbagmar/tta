b853e6bdae0735a12dd833e1176f883e92c75c36
Anand Bagmar <abagmar@gmail.com>
2012-12-21 14:14:13 +0530

    Anand: Refactor the db/seeds.rb file. Better and more meaningful names for variables used

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,44 +22,52 @@ require_relative 'common_seed_data'
 count = 0
 
 
-1.upto(2) do |i|
-  project = Project.create(:name => "PROJECT #{i}",
+def create_test_meta_data(count, project_number, sub_project_number, k)
+  test_meta_data = TestMetadatum.create(
+      :id => k,
+      :ci_job_name => SAMPLE_CI_JOB_NAMES[rand(SAMPLE_CI_JOB_NAMES.length)],
+      :os_name => SAMPLE_OS_TYPES[rand(SAMPLE_OS_TYPES.length)],
+      :host_name => SAMPLE_HOST_NAMES[rand(SAMPLE_HOST_NAMES.length)],
+      :browser => SAMPLE_BROWSER_TYPES[rand(SAMPLE_BROWSER_TYPES.length)],
+      :type_of_environment => SAMPLE_TEST_ENVIRONMENTS[rand(SAMPLE_TEST_ENVIRONMENTS.length)],
+      :date_of_execution => Time.at(rand * Time.now.to_i),
+      :test_category => SAMPLE_TEST_CATEGORIES[count],
+      :test_report_type => SAMPLE_TEST_REPORT_TYPES[rand(SAMPLE_TEST_REPORT_TYPES.length)])
+  test_meta_data.sub_project_id= (project_number*2)-2+sub_project_number
+  test_meta_data.save
+  count = count + 1
+end
+
+def create_project(project_number)
+  project = Project.create(:name => "PROJECT #{project_number}",
                            :authorization_level => "ALL")
   project.save
+end
 
-  1.upto(2) do |j|
-    sub_project= SubProject.create(:name => "Sub_project #{i}.#{j}")
-    sub_project.project_id= i
-    sub_project.save
+def create_sub_project(project_number, sub_project_number)
+  sub_project= SubProject.create(:name => "Sub_project #{project_number}.#{sub_project_number}")
+  sub_project.project_id= project_number
+  sub_project.save
+end
 
-    1.upto(2) do  |k|
+def create_test_record(test_meta_data_number, test_record_number, project_number, sub_project_number)
+  test_record = TestRecord.create(:class_name => "Class #{project_number}.#{sub_project_number}.#{test_meta_data_number}.#{test_record_number}", :number_of_tests => rand(25..50), :number_of_errors => rand(12), :number_of_failures => rand(12), :time_taken => rand(1..5).to_s)
+  test_record.test_metadatum_id= (project_number*4)-4+(sub_project_number*2)-2+test_meta_data_number
+  test_record.save
+end
+
+1.upto(2) do |project_number|
+  create_project(project_number)
+  1.upto(2) do |sub_project_number|
+    create_sub_project(project_number, sub_project_number)
+    1.upto(3) do  |test_meta_data_number|
       if count >= 3
         count=0
       end
-      meta_data = TestMetadatum.create(
-          :id => k ,:ci_job_name => SAMPLE_CI_JOB_NAMES[rand(SAMPLE_CI_JOB_NAMES.length)] ,
-          :os_name => SAMPLE_OS_TYPES[rand(SAMPLE_OS_TYPES.length)] ,
-          :host_name => SAMPLE_HOST_NAMES[rand(SAMPLE_HOST_NAMES.length)],
-          :browser => SAMPLE_BROWSER_TYPES[rand(SAMPLE_BROWSER_TYPES.length)],
-          :type_of_environment => SAMPLE_TEST_ENVIRONMENTS[rand(SAMPLE_TEST_ENVIRONMENTS.length)],
-          :date_of_execution => Time.at(rand * Time.now.to_i) ,
-          :test_category => SAMPLE_TEST_CATEGORIES[count],
-          :test_report_type => SAMPLE_TEST_REPORT_TYPES[rand(SAMPLE_TEST_REPORT_TYPES.length)]  )
-      meta_data.sub_project_id= (i*2)-2+j
-      count = count + 1
-      meta_data.save
-
-      1.upto(2) do |l|
-
-        test_record = TestRecord.create(:class_name => "Class #{i}.#{j}.#{k}.#{l}",:number_of_tests => rand(25..50),:number_of_errors =>rand(12) , :number_of_failures => rand(12),:time_taken => rand(1..5).to_s)
-        test_record.test_metadatum_id= (i*4)-4+(j*2)-2+k
-        test_record.save
-
-
-
+      count = create_test_meta_data(count, project_number, sub_project_number, test_meta_data_number)
+      1.upto(3) do |test_record_number|
+        create_test_record(test_meta_data_number, test_record_number, project_number, sub_project_number)
       end
-
     end
   end
-
 end
