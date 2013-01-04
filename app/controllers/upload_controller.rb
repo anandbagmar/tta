e43d161421499565aae5b9c6e8e2fa5b6c60e8e1
aasawaree <ritu.21090@gmail.com>
2013-01-04 12:33:02 +0530

    aasawaree/pooja: Uploaded files via UI and parsed them, created directory structure

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -7,7 +7,7 @@ class UploadController < ApplicationController
   def create
     meta_datum, project, sub_project = create_or_update_meta_datum_and_dependency
     if meta_datum.save
-      download_and_parse(meta_datum)
+      save_log_files(meta_datum)
       redirect_to :action => :show, :project_id => project.id, :sub_project_id => sub_project.id, :project_meta_id => meta_datum.id
     else
       flash[:project_error] = project.errors.messages
@@ -34,26 +34,33 @@ class UploadController < ApplicationController
   def create_or_update_meta_datum_and_dependency
     project = Project.find_or_create_by_name(params[:project_name].upcase)
     sub_project = project.sub_projects.find_or_create_by_name(params[:sub_project_name].upcase)
-    meta_datum = sub_project.test_metadatum.find_or_create_by_ci_job_name_and_browser_and_type_of_environment_and_host_name_and_os_name_and_test_category_and_test_report_type(params[:ci_job_name].upcase,params[:browser].upcase, params[:type_of_environment].upcase, params[:host_name].upcase, params[:os_name].upcase, params[:test_category].upcase, params[:test_report_type].upcase)
-    meta_datum.date_of_execution= params[:test_metadatum][:date_of_execution]
+    meta_datum = sub_project.test_metadatum.find_or_create_by_ci_job_name_and_browser_and_type_of_environment_and_host_name_and_os_name_and_test_category_and_test_report_type(params[:ci_job_name].upcase,
+                                                                                                                                                                               params[:browser].upcase, params[:type_of_environment].upcase, params[:host_name].upcase, params[:os_name].upcase, params[:test_category].upcase, params[:test_report_type].upcase)
+    meta_datum.date_of_execution= params[:date_of_execution]
     return meta_datum, project, sub_project
   end
 
-  def download_and_parse(meta_datum)
-    p params[:logDirectory].class
-    p params[:logDirectory]
-    tmp = params[:logDirectory].tempfile
-    dir_path = "/Users/khushal/Uploaded_logs/"+params[:project_name]+Time.now.strftime("%d-%m-%y:%I:%M:%S")
-    Dir.mkdir(dir_path,0777)
+  def save_log_files(meta_datum)
+    #tmp = params[:logDirectory].tempfile
+    dir_path = create_directory_structure()
     file = File.join(dir_path, params[:logDirectory].original_filename)
-    FileUtils.cp tmp.path, file
+    FileUtils.cp params[:logDirectory].tempfile.path, file
     Zip::ZipFile.open(file) do |zipFile|
       zipFile.each do |entry|
         contents = zipFile.read(entry)
         parse_xml(contents, meta_datum.id)
-
       end
     end
+  end
+
+  def create_directory_structure
+    dir_path = "/home/administrator/Documents/tta/log_files"+params[:project_name]
+    Dir.mkdir(dir_path, 0777)
+    dir_path = dir_path+"/"+params[:sub_project_name]
+    Dir.mkdir(dir_path, 0777)
+    dir_path = dir_path+"/"+Time.now.strftime("%d-%m-%y-%I:%M:%S")
+    Dir.mkdir(dir_path, 0777)
+    dir_path
   end
 
   def parse_xml(config_xml, meta_id)
@@ -67,10 +74,7 @@ class UploadController < ApplicationController
     @xml_data.time_taken= config['time']
     @xml_data.save
 
-
-
-
-
+  end
 
 end
 
