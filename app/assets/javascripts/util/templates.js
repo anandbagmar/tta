97d197bc6546e73b99c2b3f950e61c68ebad9002
Vishnu Narang <vishnun@thoughtworks.com>
2013-01-17 17:09:44 +0530

    pyramid view enhancement. Still not complete

--- app/assets/javascripts/util/templates.js
+++ app/assets/javascripts/util/templates.js
@@ -1,28 +1,11 @@
 var Template = function(){
     var templates = {
-        legendItem:'<div id={{TestType}}><div id={{TestTypeRect}}></div><span>{{TestTypeTitle}}</span></div>',
-        pyramidItem:'<div id={{TestTypeRegion}}><span id={{TestTypePercent}}>{{PercentValue}} %</span><span id={{TestTypeDuration}}>{{DurationValue}} Mins</span><div id={{TestTypeTest}}></div></div>'
-//                                                    <div id="integration_region">
-//                                                        <% if @percent_integration_test!="0.00" %>
-//                                                            <p id="integration_percent">
-//                                                                <%= @percent_integration_test %>%
-//                                                                    </p>
-//                                                                        <p id="integration_duration">
-//                                                                            <%= @duration_integration %>Mins
-//                                                                                </p>
-//                                                                                    <% end %>
-//                                                                                        <div id="integration_test">
-//                                                                                            <div id="functional_region">
-//                                                                                                <% if @percent_functional_test!="0.00" %>
-//                                                                                                    <p id="functional_percent"><%= @percent_functional_test %>%</p>
-//                                                                                                    <p id="functional_duration"><%= @duration_functional %>Mins</p>
-//                                                                                                <% end %>
-//                                                                                                    <div id="functional_test"></div>
-//                                                                                            </div>
-//                                                                                        </div>
-//                                                </div>
-//                                            </div>
-//            </div>
+        legendItem:'<div class="test-legend" id={{TestType}}><div class="rect" id={{TestTypeRect}}></div><span>{{TestTypeTitle}}</span></div>',
+        pyramidItem:'<div class="test-region" id={{TestTypeRegion}}>' +
+            '<span id={{TestTypePercent}}>{{PercentValue}} %</span>' +
+            '<span id={{TestTypeDuration}}>{{DurationValue}} Mins</span>' +
+            '<div class="test-triangle" id={{TestTypeTest}}></div>' +
+          '</div>'
     };
     return {
         render: function(template,data){
