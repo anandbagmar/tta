cabaaa459df20e8fba80c8f46fb0fcbd2781b557
Vishnu Narang <vishnun@thoughtworks.com>
2013-01-17 18:48:21 +0530

    Vishnu - Fixed UI defects on Pyramid view

---
+++ app/assets/javascripts/util/utils.js
@@ -0,0 +1,9 @@
+var Utils={};
+Utils.getRandomColor = function(){
+  var letters = '0123456789ABCDEF'.split('');
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+    color += letters[Math.round(Math.random() * 15)];
+  }
+  return color;
+};
