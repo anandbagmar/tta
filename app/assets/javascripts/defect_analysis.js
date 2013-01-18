6cad2450053ad51fe02605394b8a50c302d507bb
Vishnu Narang <vishnun@thoughtworks.com>
2013-01-18 19:42:12 +0530

    Vishnu - Defect Analysis View implemented. First cut.

---
+++ app/assets/javascripts/defect_analysis.js
@@ -0,0 +1,35 @@
+$("document").ready(function(){
+  var jsonData=$("#defect-analysis-data").text(),
+      subProjectName = "";
+  if(jsonData != ""){
+    jsonData = JSON.parse(jsonData);
+    subProjectName = "<h1>"+jsonData.sub_project_name.toUpperCase()+"</h1>";
+    var errors = jsonData.errors,
+        templates = new Template(),
+        errorListData = "",
+        errorMessageData = "",
+        defectErrorMessageTemplate = templates.getDefectErrorMessagesTemplate(),
+        errorListTemplate = templates.getErrorListTemplate();
+    for(var errorName in errors){
+      if(errorName=="")
+        continue;
+      var errorMessages = errors[errorName];
+      errorMessageData = "";
+      for(index = 0,len=errorMessages.length;index<len;index++){
+        errorMessageData += "<li>"+errorMessages[index]+"</li>";
+      }
+
+      errorMessageData = templates.render(defectErrorMessageTemplate,{
+        "ErrorName": errorName,
+        "ErrorMessageList": errorMessageData.replace("'"," ")
+      });
+      errorListData += "<li class='clearfix'>"+errorMessageData+"</li>";
+    }
+    errorListData = templates.render(errorListTemplate,{
+      "ErrorList": errorListData
+    });
+
+    $("#defect-analysis-table").html(subProjectName + errorListData);
+  }
+  return false;
+});
--- app/assets/javascripts/util/templates.js
+++ app/assets/javascripts/util/templates.js
@@ -1,11 +1,13 @@
 var Template = function(){
     var templates = {
-        legendItem:'<div class="test-legend" id={{TestType}}><div class="rect" id={{TestTypeRect}}></div><span>{{TestTypeTitle}}</span></div>',
-        pyramidItem:'<div class="test-region" id={{TestTypeRegion}}>' +
-            '<span id={{TestTypePercent}}>{{PercentValue}} %</span>' +
-            '<span id={{TestTypeDuration}}>{{DurationValue}} Seconds</span>' +
-            '<div class="test-triangle" id={{TestTypeTest}}></div>' +
-          '</div>'
+      legendItem:'<div class="test-legend" id={{TestType}}><div class="rect" id={{TestTypeRect}}></div><span>{{TestTypeTitle}}</span></div>',
+      pyramidItem:'<div class="test-region" id={{TestTypeRegion}}>' +
+          '<span id={{TestTypePercent}}>{{PercentValue}} %</span>' +
+          '<span id={{TestTypeDuration}}>{{DurationValue}} Seconds</span>' +
+          '<div class="test-triangle" id={{TestTypeTest}}></div>' +
+        '</div>',
+      errorListTemplate:'<ul class="error_list">{{ErrorList}}</ul>',
+      defectErrorMessagesTemplate:'<span>{{ErrorName}}</span><div class="error_messages"><ul>{{ErrorMessageList}}<ul></div>'
     };
     return {
         render: function(template,data){
@@ -21,6 +23,12 @@ var Template = function(){
         },
         getPyramidItem: function(){
             return templates.pyramidItem;
+        },
+        getDefectErrorMessagesTemplate:function(){
+          return templates.defectErrorMessagesTemplate;
+        },
+        getErrorListTemplate: function(){
+          return templates.errorListTemplate;
         }
     };
 };
