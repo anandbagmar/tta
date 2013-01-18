35292c6011f3db619fe6838a4c6f3ff80ef54dcb
pooja-salpekar <salpekar.pooja4@gmail.com>
2013-01-18 18:12:26 +0530

    Pooja: removed nil entries of error msg from json

--- app/models/defect_analysis.rb
+++ app/models/defect_analysis.rb
@@ -23,13 +23,15 @@ class DefectAnalysis
   def self.get_test_cases(result)
     test_cases=[]
     result.each do |test_suite_id|
-       test_cases << TestCaseRecord.find_by_test_suite_record_id(test_suite_id ,:select=> "class_name , error_msg")
+       test_cases << TestCaseRecord.find_by_test_suite_record_id(test_suite_id ,:select=> "class_name , error_msg",:conditions => "error_msg IS NOT NULL")
     end
     result_hash = {}
     test_cases.each do |test_case|
-      error_message = test_case.error_msg
-      result_hash[error_message] = [] unless result_hash.keys.include?(error_message)
-      result_hash[error_message] << test_case.class_name
+      if !(test_case.nil?)
+        error_message = test_case.error_msg
+        result_hash[error_message] = [] unless result_hash.keys.include?(error_message)
+        result_hash[error_message] << test_case.class_name unless test_case.nil?
+      end
     end
     return result_hash
   end
