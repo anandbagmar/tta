1099962a9ffe6c4286a0f91857d28398dc890b44
pooja-salpekar <salpekar.pooja4@gmail.com>
2013-01-18 19:09:29 +0530

    Pooja: added validation on defect_analysis view

--- app/controllers/defect_analysis_controller.rb
+++ app/controllers/defect_analysis_controller.rb
@@ -13,10 +13,24 @@ class DefectAnalysisController < ApplicationController
 
   end
   def sub_project_filter
+    flash[:no_id_error]=nil
+    flash[:no_date_error]=nil
+    error_flag=0;
     sub_project_id=params[:sub_project][:id]
-    analysis_date=params[:comparative_analysis][:analysis_date]
-    @defect_analysis_json = DefectAnalysis.getResultJson(sub_project_id,analysis_date)
-    render :create
+    analysis_date=params[:defect_analysis][:analysis_date]
+    if sub_project_id.blank?
+      flash[:no_id_error] = "No Sub-Project Selected"
+      error_flag=1
+    end
+    if analysis_date==""
+      flash[:no_date_error]="No Date Selected"
+      error_flag=1
+    end
+    if error_flag ==1
+      render 'defect_analysis/new'
+    else
+      @defect_analysis_json = DefectAnalysis.getResultJson(sub_project_id,analysis_date)
+      render :create
+    end
   end
-
 end
