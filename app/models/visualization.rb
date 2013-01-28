530a25a0092ed214b7363b2c032ee86edef01681
pooja-salpekar <salpekar.pooja4@gmail.com>
2013-01-28 18:13:27 +0530

    Pooja: sorting of pyramid data by percent of test passed

--- app/models/visualization.rb
+++ app/models/visualization.rb
@@ -1,8 +1,6 @@
 class Visualization
 
   def self.getResultJson(sub_project_id)
-
-
     getAllFields(sub_project_id)
     test_types=[]
     @test_types.zip(@percent_of_tests, @duration_of_tests).each do |test_type, percent_of_test, duration_of_test|
@@ -13,39 +11,17 @@ class Visualization
           :duration => duration_of_test
         }
     end
+
     @json = {
       :sub_project_name => SubProject.find(sub_project_id, :select => "name").name,
-      :test_types => test_types
+      :test_types => test_types.sort_by{|test_type|test_type[:percent]}
     }.to_json
-
     return @json
   end
 
   private
-  def self.calculatePercentageAndDuration(result)
-    no_of_tests=[]; duration_of_tests=[]; percentOfTests=[];
-    total=0.0
-    result.each do |test_type_result|
-      no_of_test_of_test_type = 0
-      duration_of_test_of_test_type =0.0
-      test_type_result.each do |test_type|
-        no_of_test_of_test_type += test_type[0]
-        duration_of_test_of_test_type += (test_type[1].to_f)/1000
-      end
-      duration_of_tests<<"%0.6f" %(duration_of_test_of_test_type)
-      no_of_tests << no_of_test_of_test_type
-    end
-    no_of_tests.each do |entry|
-      total += entry
-    end
-    no_of_tests.each do |entry|
-      percentOfTests << "%0.2f" %((entry.to_f/total)*100)
-    end
-    return percentOfTests, duration_of_tests
-  end
-
-
   def self.getAllFields(sub_project_id)
+
     @test_types = TestMetadatum.find_all_by_sub_project_id(sub_project_id, :select => "DISTINCT(test_category)")
     result =[]
     @test_types.each do |test_type|
@@ -67,4 +43,25 @@ class Visualization
     }
   end
 
+  def self.calculatePercentageAndDuration(result)
+    no_of_tests=[]; duration_of_tests=[]; percentOfTests=[];
+    total=0.0
+    result.each do |test_type_result|
+      no_of_test_of_test_type = 0
+      duration_of_test_of_test_type =0.0
+      test_type_result.each do |test_type|
+        no_of_test_of_test_type += test_type[0]
+        duration_of_test_of_test_type += (test_type[1].to_f)/1000
+      end
+      duration_of_tests<<"%0.6f" %(duration_of_test_of_test_type)
+      no_of_tests << no_of_test_of_test_type
+    end
+    no_of_tests.each do |entry|
+      total += entry
+    end
+    no_of_tests.each do |entry|
+      percentOfTests << "%0.2f" %((entry.to_f/total)*100)
+    end
+    return percentOfTests, duration_of_tests
+  end
 end
--- spec/models/visualization_spec.rb
+++ spec/models/visualization_spec.rb
@@ -30,6 +30,20 @@ describe Visualization do
     json.should eq("{\"sub_project_name\":\"SubProject1.1\",\"test_types\":[{\"test_name\":\"UNIT TEST\",\"percent\":\"100.00\",\"duration\":\"0.005000\"}]}")
   end
 
+  it "should return json with test_types sorted by percent" do
+    project = FactoryGirl.create(:project, :name => "ProjectDummy", :authorization_level => "NULL")
+    sub_project = FactoryGirl.create(:sub_project, :project_id => project.id,:name => "SubProject1.1")
+    test_metadata = FactoryGirl.create(:test_metadatum,:sub_project_id => sub_project.id,:ci_job_name => "build", :os_name=> "fedora" , :host_name => "pooja-pc" , :browser => "firefox" , :type_of_environment =>"Dev" ,:date_of_execution => "2012-12-12",:test_category=>"UNIT TEST" ,:test_report_type => "JUnit")
+    test_metadata_functional = FactoryGirl.create(:test_metadatum,:sub_project_id => sub_project.id,:ci_job_name => "build", :os_name=> "fedora" , :host_name => "pooja-pc" , :browser => "firefox" , :type_of_environment =>"Dev" ,:date_of_execution => "2012-12-12",:test_category=>"FUNCTIONAL TEST" ,:test_report_type => "JUnit")
+    test_suite_record = FactoryGirl.create(:test_suite_records,:test_metadatum_id => test_metadata.id, :class_name => "Class1.1",:number_of_tests => 800 , :number_of_errors=>2 ,:number_of_failures =>2,:time_taken=>5)
+    test_suite_record_functional = FactoryGirl.create(:test_suite_records,:test_metadatum_id => test_metadata_functional.id, :class_name => "Class1.1",:number_of_tests => 100 , :number_of_errors=>2 ,:number_of_failures =>2,:time_taken=>5)
+    test_case_record = FactoryGirl.create(:test_case_record,:test_suite_record_id => test_suite_record.id,:time_taken=>1,:class_name=>"class1.1.1",:error_msg=>"")
+    test_case_record_functional = FactoryGirl.create(:test_case_record,:test_suite_record_id => test_suite_record_functional.id,:time_taken=>1,:class_name=>"class1.1.1",:error_msg=>"")
+    json= Visualization.getResultJson(sub_project.id)
+    parsed_json = ActiveSupport::JSON.decode(json)
+    parsed_json["test_types"][0]["percent"].should be < parsed_json["test_types"][1]["percent"]
+  end
+
   it "should return json with proper sub_project_name" do
     project = FactoryGirl.create(:project, :name => "ProjectDummy", :authorization_level => "NULL")
     sub_project = FactoryGirl.create(:sub_project, :project_id => project.id,:name => "SubProject1.1")
