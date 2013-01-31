3fe4d7b63591bb79d3ddd957e450854e3dbe389c
Sanchari <sanchari@Sancharis-MacBook-Pro.local>
2013-01-31 15:56:37 +0530

    sanchari/priti:files added and renamed

--- features/step_definitions/comparative_analysis.rb
+++
@@ -1,5 +0,0 @@
-
-Given /^The User uploads data with following attributes$/ do |table|
-  # table is a |Project1|SubProj1|Run unit test|Mac    |Sailee   |IE     |DEV        |2012-01-01       |/var/lib/go-agent/pipelines/Development/tta_spec_results.zip |Unit Test       |pending
-  upload_the_data(table)
-end
