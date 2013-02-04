24b2541ee80e99ec42c7b6a11730699d20a632ee
Sanchari <sanchari@Sancharis-MacBook-Pro.local>
2013-02-04 17:35:02 +0530

    Sanchari/Pooja: fixed date being sent as fixnum in C_A spec

--- spec/models/comaprative_analysis_spec.rb
+++ spec/models/comaprative_analysis_spec.rb
@@ -3,12 +3,12 @@ require "rspec"
 describe ComparativeAnalysis do
 
   it "should return result set" do
-    result = ComparativeAnalysis.get_result_set(1,2012-1-1,2012-12-12)
+    result = ComparativeAnalysis.get_result_set(1,"2012-1-1","2012-12-12")
     result.should_not be_nil
   end
 
   it"should throw error if sub_project id not given" do
- expect{ComparativeAnalysis.get_result_set(nil)}.to raise_error
+ expect{ComparativeAnalysis.get_result_set(nil,nil,nil)}.to raise_error
   end
 
 end
