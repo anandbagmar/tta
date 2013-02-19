654a0125cc99e3ffe1e528fba159263cd3e62157
Pranjali <pranjalitalekar.19@gmail.com>
2013-02-19 15:32:15 +0530

    Pranjali: added web analytics functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,18 @@
 //= require jquery
 //= require jquery.ui.all
 //= require_tree .
+
+
+
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-38578491-1']);
+_gaq.push(['_trackPageview']);
+_gaq.push(['_setDomainName', 'none']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
