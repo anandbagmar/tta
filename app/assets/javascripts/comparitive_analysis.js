00be4b07f726f99ad1c4e4a42874cdc87b588ef3
aasawaree <ritu.21090@gmail.com>
2013-02-27 01:18:12 +0530

    [Aasawaree] Modified CSS for error messages on blank fields

--- app/assets/javascripts/comparitive_analysis.js
+++ app/assets/javascripts/comparitive_analysis.js
@@ -3,8 +3,8 @@ var ValidateStartDateIsLessThanEndDate={
     checkForDates : function($form){
 
         var valid =true;
-        var start_date = $form.find(".date-field").find('#comparative_analysis_start_date').val();
-        var end_date = $form.find(".date-field").find('#comparative_analysis_end_date').val();
+        var start_date = $form.find(".required-field-date-field").find('#comparative_analysis_start_date').val();
+        var end_date = $form.find(".required-field-date-field").find('#comparative_analysis_end_date').val();
 
         if(start_date>=end_date && start_date!="" && end_date!="")
             valid=false;
