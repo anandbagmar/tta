a9451f9d218336b3e6c73b8ec00a9f09d674ae0a
sailee <sailee@dell6320.corporate.thoughtworks.com>
2013-02-26 14:43:55 +0530

    [Sailee/Aasawaree]: Added js for date validation on comparative analysis page and removed the flash messages and rspecs for the same

--- app/assets/javascripts/comparitive_analysis.js
+++ app/assets/javascripts/comparitive_analysis.js
@@ -1,31 +1,23 @@
-$(document).ready(function(){
-  validateForm = function(form){
-    var requiredFields = $('#'+form.FormId + " .required-field"),
-        index, validFlag = true;
-    for(index=0,len=requiredFields.length;index<len;index++){
-      var $fieldParent = $(requiredFields[index]),
-          field = $fieldParent.children()[0],
-          errorMsg = $fieldParent.children('span');
-      if(field.value==""){
-        $(field).css({
-          outline:"1px solid red",
-          outlineStyle: "dotted",
-          outlineOffset: "-1px"
-        });
-        $(errorMsg).show();
-        validFlag = false;
-      }
-      else{
-        $(field).css({outline:"0px solid red"});
-        $(errorMsg).hide();
-      }
-    }
-    return validFlag;
-  };
-  $("#submit-btn").on('click',function(e){
-    if(validateForm({'FormId':'comparative-analysis-form'}))
-      $("#form-submit").trigger('click');
-    return false;
-  });
+var ValidateStartDateIsLessThanEndDate={
 
-});
+    checkForDates : function($form){
+
+        var valid =true;
+        var start_date = $form.find(".date-field").find('#comparative_analysis_start_date').val();
+        var end_date = $form.find(".date-field").find('#comparative_analysis_end_date').val();
+
+        if(start_date>=end_date)
+            valid=false;
+
+        var errMsg = $form.children('span');
+
+        if(valid==false)
+            $(errMsg).show();
+        else
+            $(errMsg).hide();
+
+        return valid;
+    }
+};
+
+
--- app/controllers/comparative_analysis_controller.rb
+++ app/controllers/comparative_analysis_controller.rb
@@ -20,26 +20,14 @@ class ComparativeAnalysisController < ApplicationController
     @end_date= params[:comparative_analysis][:end_date]
     @project_name = Project.find_by_id(params[:project][:id]).name
     flash[:no_data_error]=nil
-    flash[:date_error]=nil
-    if (@start_date=="" or @end_date=="" or params[:project][:id]=="")
-      flash[:required_field] = "This Field is required."
-      render :create
-    else
-      if (@start_date >= @end_date)
-        flash[:date_error]="select end date greater than start date"
-      else
-        @result_set = ComparativeAnalysis.get_result_set(params[:project][:id], @start_date, @end_date)
-        flash[:required_field]=""
-      end
+    @result_set = ComparativeAnalysis.get_result_set(params[:project][:id], @start_date, @end_date)
       if !(@result_set.nil?)
         if @result_set[@result_set.keys[0]].empty?
         flash[:no_data_error]="No Data between the Date Range "+@start_date+" To "+@end_date
         end
       end
       render :create
-    end
   end
-
 end
 
 
