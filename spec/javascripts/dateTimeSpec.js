877db7f007e9bec4e08efa0ee4c9db22de3d1f02
aasawaree <ritu.21090@gmail.com>
2013-02-26 11:22:39 +0530

    [Aasawaree] Modified jasmine test

--- spec/javascripts/dateTimeSpec.js
+++ spec/javascripts/dateTimeSpec.js
@@ -16,7 +16,7 @@ describe("ValidateDate",function(){
         var valid = validateDate.checkForValidDate($("#uploadForm"));
         expect(valid).toBe(true);
     });
-    it("returns false on wrong date for February during a non-leap year", function(){
+    it("returns false on wrong date for February during a leap year", function(){
         loadFixtures("feb_leap_year_invalid_date.html");
         var valid = validateDate.checkForValidDate($("#uploadForm"));
         expect(valid).toBe(false);
@@ -32,8 +32,3 @@ describe("ValidateDate",function(){
         expect(valid).toBe(false);
     });
 });
-
-
-//        $(".date-field").find("#date_year option[value='2014']").attr("selected","selected");
-//        $(".date-field").find("#date_month option[value='02']").attr("selected","selected");
-//        $(".date-field").find("#date_day option[value='38']").attr("selected","selected");
