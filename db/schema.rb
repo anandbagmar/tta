3633ed49c3e44bccbe51436856f15fff3e0a9c4a
sailee <sailee@dell6320.corporate.thoughtworks.com>
2013-02-28 14:59:52 +0530

    [Sailee]: Removed the schema for admin model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130220134122) do
-
-  create_table "admins", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 20130215051840) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
