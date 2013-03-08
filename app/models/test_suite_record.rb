98d443e0739c223a71f736868a20f9ca0ce523ed
Priti <priti.biyani6@gmail.com>
2013-03-08 11:01:31 +0530

    [Priti] method added for getting uniq class_names from test_suites

--- app/models/test_suite_record.rb
+++ app/models/test_suite_record.rb
@@ -3,4 +3,10 @@ class TestSuiteRecord < ActiveRecord::Base
   belongs_to :test_metadatum
   attr_accessible :class_name, :number_of_errors, :number_of_failures, :number_of_tests, :time_taken,:number_of_tests_not_run,:number_of_tests_ignored
 
+  def self.get_uniq_class_names_between_two_runs (metadata_one_id,metadata_two_id)
+     metadata_ids=[metadata_one_id,metadata_two_id]
+     @class_names=TestSuiteRecord.find_all_by_test_metadatum_id(metadata_ids).map{|record| record.class_name}.uniq
+  end
+
+
 end
