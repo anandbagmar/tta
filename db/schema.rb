c17489542c740f790027535b8aaa58e477cdb53d
Anand Bagmar <abagmar@gmail.com>
2013-04-02 15:51:36 +0530

    Anand - more indentation fixes

--- db/schema.rb
+++ db/schema.rb
@@ -14,55 +14,55 @@
 ActiveRecord::Schema.define(:version => 20130215051840) do
 
   create_table "projects", :force => true do |t|
-    t.string "name"
-    t.string "authorization_level"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "name"
+    t.string   "authorization_level"
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
   end
 
   create_table "sub_projects", :force => true do |t|
-    t.integer "project_id"
-    t.string "name"
+    t.integer  "project_id"
+    t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
 
   create_table "test_case_records", :force => true do |t|
-    t.string "class_name"
-    t.string "time_taken"
-    t.integer "test_suite_record_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.text "error_msg"
+    t.string   "class_name"
+    t.string   "time_taken"
+    t.integer  "test_suite_record_id"
+    t.datetime "created_at",           :null => false
+    t.datetime "updated_at",           :null => false
+    t.text     "error_msg"
   end
 
   add_index "test_case_records", ["test_suite_record_id"], :name => "index_test_case_records_on_test_suite_record_id"
 
   create_table "test_metadata", :force => true do |t|
-    t.integer "sub_project_id"
-    t.string "ci_job_name"
-    t.string "os_name"
-    t.string "host_name"
-    t.string "browser"
-    t.string "type_of_environment"
+    t.integer  "sub_project_id"
+    t.string   "ci_job_name"
+    t.string   "os_name"
+    t.string   "host_name"
+    t.string   "browser"
+    t.string   "type_of_environment"
     t.datetime "date_of_execution"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.string "test_category"
-    t.string "test_report_type"
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
+    t.string   "test_category"
+    t.string   "test_report_type"
   end
 
   create_table "test_suite_records", :force => true do |t|
-    t.integer "test_metadatum_id"
-    t.string "class_name"
-    t.integer "number_of_tests"
-    t.integer "number_of_errors"
-    t.integer "number_of_failures"
-    t.string "time_taken"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.integer "number_of_tests_not_run"
-    t.integer "number_of_tests_ignored"
+    t.integer  "test_metadatum_id"
+    t.string   "class_name"
+    t.integer  "number_of_tests"
+    t.integer  "number_of_errors"
+    t.integer  "number_of_failures"
+    t.string   "time_taken"
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
+    t.integer  "number_of_tests_not_run"
+    t.integer  "number_of_tests_ignored"
   end
 
 end
--- spec/controllers/upload_controller_spec.rb
+++ spec/controllers/upload_controller_spec.rb
@@ -2,9 +2,7 @@ require 'spec_helper'
 
 
 describe UploadController do
-
   context "GET 'upload'" do
-
     it "returns http success" do
       get 'new'
       response.should be_success
@@ -16,15 +14,13 @@ describe UploadController do
       @file = fixture_file_upload('/proj5.zip', 'application/zip')
       @attr = {:project_name => "tta", :sub_project_name => "tta_sub", :ci_job_name => "build", :test_category => "Unit test", :test_report_type => "JUnit", :date => {"year"=>"2012", "month"=>"5", "day"=>"26", "hour"=>"07", "minute"=>"46"},
                :browser => "firefox", :host_name => "host_pc", :os_name => "mac-osx", :type_of_environment => "dev", :logDirectory => @file}
-
     end
-    it "uploads data and redirect to success page" do
 
+    it "uploads data and redirect to success page" do
       post :create, @attr
       create_or_load_project_if_not_present()
 
       response.should redirect_to upload_show_path(:project_id => @project.id, :project_meta_id => @meta_data.id, :sub_project_id => @sub_project.id)
-
     end
 
     it "check success page shows the project details" do
@@ -38,7 +34,6 @@ describe UploadController do
       assigns[:sub_project].should == @sub_project
       assigns[:project_meta].should == @meta_data
       flash[:notice].should == "Project Successfully Saved!!"
-
     end
 
     def create_or_load_project_if_not_present
@@ -46,11 +41,7 @@ describe UploadController do
       @sub_project = @project.sub_projects.find_or_create_by_name("tta_sub")
       @meta_data = TestMetadatum.find_or_create_by_sub_project_id(@sub_project.id)
     end
-
-
-
   end
-
 end
 
 
