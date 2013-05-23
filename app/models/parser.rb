b2adf1b098c2647cec3f374ad9619c4fe04bff18
aasawaree <ritu.21090@gmail.com>
2013-05-23 16:04:28 +0530

    aasawaree/anand/anjali: renaming config_xml to config_..

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -31,30 +31,30 @@ class Parser
     #JasmineParser.new.parse(extracted_file_content, meta_datum_id, test_report_type)
   end
 
-  def parse_test_run_record_nunit_xml(meta_id, test_report_type, config_xml)
+  def parse_test_run_record_nunit_xml(meta_id, test_report_type, extracted_xml)
     return if (REPORTTYPE["test_report_type_mapping"][test_report_type.downcase] != "xml")
-    UnitNunitParser.parse(config_xml, meta_id)
+    UnitNunitParser.parse(extracted_xml, meta_id)
   end
 
-  def parse_test_run_record_html(meta_id, test_report_type, config_html)
+  def parse_test_run_record_html(meta_id, test_report_type, extracted_html)
     return if (REPORTTYPE["test_report_type_mapping"][test_report_type.downcase] != "html")
     if test_report_type == "Cucumber HTML"
-      CucumberHtmlParser.parse(config_html, meta_id)
+      CucumberHtmlParser.parse(extracted_html, meta_id)
     end
   end
 
-  def parse_test_run_record_xml(meta_id, test_report_type, config_xml)
+  def parse_test_run_record_xml(meta_id, test_report_type, extracted_xml)
     return if (REPORTTYPE["test_report_type_mapping"][test_report_type.downcase] != "xml")
 
-    @doc = Nokogiri::XML config_xml
+    @doc = Nokogiri::XML extracted_xml
 
     test_suites = @doc.xpath("//testsuite")
     test_suites.each do |test_suite|
-      save_test_suite(meta_id, test_report_type, config_xml, test_suite)
+      save_test_suite(meta_id, test_report_type, extracted_xml, test_suite)
     end
   end
 
-  def save_test_suite(meta_id, test_report_type, config_xml, test_suite)
+  def save_test_suite(meta_id, test_report_type, extracted_xml, test_suite)
     test_record_to_be_created = {:time_taken => 0.0, :test_metadatum_id => meta_id, :class_name => test_suite.attr("name"), :number_of_tests => test_suite.attr("tests"),
                                  :number_of_errors => test_suite.attr("errors"), :number_of_failures => test_suite.attr("failures")}
     @doc.xpath("//testsuite/testcase").each do |test_case|
@@ -62,15 +62,15 @@ class Parser
       test_record_to_be_created[:time_taken] += time.to_f
     end
     saved_test_suite_data = TestSuiteRecord.create_and_save(test_record_to_be_created)
-    save_test_case(test_suite, saved_test_suite_data, test_report_type, config_xml)
+    save_test_case(test_suite, saved_test_suite_data, test_report_type, extracted_xml)
 
   end
 
-  def save_test_case(test_suite, saved_test_suite_data, test_report_type, config_xml)
+  def save_test_case(test_suite, saved_test_suite_data, test_report_type, extracted_xml)
     if test_report_type == "Rspec JUnit" || test_report_type == "Cucumber JUnit"
-      XmlParser.new.saving_junit_test_cases(config_xml, saved_test_suite_data, test_report_type, test_suite)
+      XmlParser.new.saving_junit_test_cases(extracted_xml, saved_test_suite_data, test_report_type, test_suite)
     elsif test_report_type == "Groovy NUnit"
-      GroovyNunitParser.parse(config_xml, saved_test_suite_data, test_suite)
+      GroovyNunitParser.parse(extracted_xml, saved_test_suite_data, test_suite)
     end
   end
 end
