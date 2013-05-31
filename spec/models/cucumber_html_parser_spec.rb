a3686a001404bde21227aa2d5b9f220f5581d67a
aasawaree <ritu.21090@gmail.com>
2013-05-31 13:43:33 +0530

    aasawaree: fixed failing test and changed the version number

--- spec/models/cucumber_html_parser_spec.rb
+++ spec/models/cucumber_html_parser_spec.rb
@@ -12,7 +12,7 @@ describe CucumberHtmlParser do
       FactoryGirl.create(:test_case_record,:test_suite_record_id => test_suite_record.id)
 
       result_visualization = Visualization.new.getResultJson(sub_project.id)
-      result_visualization.should eq("{\"sub_project_name\":\"TTA_subProject\",\"test_types\":[{\"test_name\":\"UNIT TEST\",\"seq_no\":101,\"percent\":\"100.00\",\"duration\":\"2.000\",\"no_of_test\":10,\"percentage_passing\":\"60.00\"}],\"unknown_test_types\":null}")
+      result_visualization.should eq("{\"sub_project_name\":\"TTA_subProject\",\"test_types\":[{\"test_name\":\"UNIT TEST\",\"seq_no\":101,\"percent\":\"100.00\",\"duration\":\"00:00:02:000\",\"no_of_test\":10,\"percentage_passing\":\"60.00\"}],\"unknown_test_types\":null}")
 
       result_comparative = ComparativeAnalysis.new.get_result_set(project.id,"2013-01-01".to_date,"2013-03-30".to_date)
       result_comparative.should eq({"TTA_subProject"=>[[1361318400000.0, 60.0]]})
