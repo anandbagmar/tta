4740752a24f6689f843ba843ea5a639dd49a3d4f
sk176h <sk176h@gmail.com>
2013-07-28 10:37:47 -0600

    Added tests to verify that TestMetadatum.get_records_for_specific_date properly distinguishes records by date , category and subproject

--- spec/models/test_metadatum_spec.rb
+++ spec/models/test_metadatum_spec.rb
@@ -32,4 +32,94 @@ describe TestMetadatum do
     end
   end
 
+  describe "get_record_for_specific_date" do 
+    include DataHelper
+    
+    let(:cat_int_test) {"INTEGRATION TESTS"}
+    let(:cat_unit_test) {"UNIT TESTS"}
+    
+    let(:project) {create_project}
+    let(:sub_project) {create_subproject_for_project project}
+    let(:be_equal_to_one) {eq(1)}
+
+    def id_of(metadata)
+      metadata.id
+    end 
+
+    def be_equal_to(other)
+      eql(other)
+    end
+
+    def length_of(metadata)
+      metadata.length
+    end
+
+    it "distinguishes records by test category"  do    
+
+      inserted_int_test_metadatatum = create_metadatum sub_project , "2013-01-01" , cat_int_test    
+      inserted_unit_test_metadatatum = create_metadatum sub_project , "2013-01-01" , cat_unit_test
+  
+      retrieved_int_test_metadata = get_metadata(
+        sub_project,
+        cat_int_test,
+        inserted_int_test_metadatatum.date_of_execution)  
+
+      retrieved_unit_test_metadata = get_metadata(
+        sub_project,
+        cat_unit_test,
+        inserted_unit_test_metadatatum.date_of_execution)
+
+      length_of(retrieved_int_test_metadata).should be_equal_to_one
+      length_of(retrieved_unit_test_metadata).should be_equal_to_one      
+      id_of(retrieved_int_test_metadata.first).should be_equal_to(id_of inserted_int_test_metadatatum)
+      id_of(retrieved_unit_test_metadata.first).should be_equal_to(id_of inserted_unit_test_metadatatum)
+
+    end
+    
+    it "distinguishes records by test execution date" do 
+
+      inserted_int_test_metadatatum1 = create_metadatum sub_project , "2013-01-01" , cat_int_test     
+      inserted_int_test_metadatatum2 = create_metadatum sub_project , "2013-01-02" , cat_int_test     
+  
+      retrieved_int_test_metadata1 = get_metadata(
+        sub_project,
+        cat_int_test,
+        inserted_int_test_metadatatum1.date_of_execution) 
+
+      retrieved_int_test_metadata2 = get_metadata(
+        sub_project,
+        cat_int_test,
+        inserted_int_test_metadatatum2.date_of_execution)
+
+      length_of(retrieved_int_test_metadata1).should be_equal_to_one
+      length_of(retrieved_int_test_metadata2).should be_equal_to_one 
+      id_of(retrieved_int_test_metadata1.first).should be_equal_to(id_of inserted_int_test_metadatatum1)
+      id_of(retrieved_int_test_metadata2.first).should be_equal_to(id_of inserted_int_test_metadatatum2)    
+    end
+
+    it "distinguishes records by sub projects" do 
+       
+      sub_project_other = create_subproject_for_project project 
+
+      inserted_sub_project_metadatum = create_metadatum sub_project , "2013-01-01" , cat_int_test
+      inserted_sub_project_other_metadatum =  create_metadatum sub_project_other , "2013-01-01" , cat_int_test
+
+      date_of_execution = inserted_sub_project_metadatum.date_of_execution
+
+      retrieved_sub_project_metadata = get_metadata(
+        sub_project,
+        cat_int_test,
+        date_of_execution) 
+
+      retrieved_sub_project_other_metadata = get_metadata(
+        sub_project_other,
+        cat_int_test,
+        date_of_execution)
+
+      length_of(retrieved_sub_project_metadata).should be_equal_to_one
+      length_of(retrieved_sub_project_other_metadata).should be_equal_to_one
+      id_of(retrieved_sub_project_metadata.first).should be_equal_to(id_of inserted_sub_project_metadatum)
+      id_of(retrieved_sub_project_other_metadata.first).should be_equal_to(id_of inserted_sub_project_other_metadatum)
+    end
+  end
 end
