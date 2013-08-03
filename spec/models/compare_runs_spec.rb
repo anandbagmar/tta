f3a418254472c74f79b58b27e33b142076663072
sk176h <sk176h@gmail.com>
2013-08-02 23:47:56 -0600

    Intermediate Commit
    intermediate commit for the compare_runs_spec.rb file

--- spec/models/compare_runs_spec.rb
+++ spec/models/compare_runs_spec.rb
@@ -64,24 +64,24 @@ end
 describe "CompareRuns" do 
 
 	include CompareRunsSpecHelper
+	
 	before(:each) do		
 		common_setup
 		create_new_project_and_subproject
-		@metadata1 = create_metadatum(@sub_project,@jan_1_2013,@unit_tests)		
-		@test_suite1 = create_suite_with_metadata(@metadata1,"rest_unit_tests")
-		@test_suite2 = create_suite_with_metadata(@metadata1,"rest_unit_tests")
+		@metadata1 = create_metadatum(@sub_project,@jan_1_2013,@unit_tests)	
+		@test_suite1 = create_suite_with_metadata(@metadata1,"rest_unit_tests")		
 	end
 
-	describe "get_test_suite_records_with_errors_for" do 
-			
-		 context "when there are no failures for a subproject and test category on a given day" do
+	describe "#get_test_suite_records_with_errors_for" do 
+
+		context "when there are no failures for a subproject and test category on a given day" do
         	it "should return no records" do
         		records_with_error_for(@sub_project,@unit_tests,@jan_1_2013).should be_empty					
         	end
         end
 
 		context "when there are failures for one test suite and test category on a given day" do			
-			before(:each) do				
+			before do				
 				create_class_errors(@test_suite1,class_error_hash_from("001","002","003"))
 				@expected_errors = class_name_arr_from("001","002","003")
 			end
@@ -91,7 +91,8 @@ describe "CompareRuns" do
 		end
 
 		context "when there are failures for multiple test suites for a test category on a given day" do 
-			before(:each) do				
+			before do				
+				@test_suite2 = create_suite_with_metadata(@metadata1,"rest_unit_tests")
 				create_class_errors(@test_suite1,class_error_hash_from("001","002","003"))
 				create_class_errors(@test_suite2,class_error_hash_from("004","005","006"))
 				@expected_errors = class_name_arr_from("001","002","003","004","005","006")
@@ -102,4 +103,15 @@ describe "CompareRuns" do
 		end
 
 	end
+
+
+	describe "#getCompareResult" do 
+
+		
+		
+
+
+	end
+
+
 end
