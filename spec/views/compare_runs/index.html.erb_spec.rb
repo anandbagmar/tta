dbba7b212ab4c7eb43c06a01fc7b8d110cbc395c
sk176h <sk176h@gmail.com>
2013-08-08 22:19:27 -0600

    Intermediate Commit
    added 1st test case for compare runs view specs
    2nd test case partially complete

--- spec/views/compare_runs/index.html.erb_spec.rb
+++ spec/views/compare_runs/index.html.erb_spec.rb
@@ -2,6 +2,15 @@ require 'spec_helper'
 require 'rspec'
 require File.expand_path("spec/helpers/comparesunsspechelper.rb")
 
+
+class Fixnum
+
+	def tests_failing
+		"Number of failing tests: #{self}"
+	end
+
+end
+
 module CompareRunsSeleniumHelper
 
 	def open_compare_runs_page
@@ -14,6 +23,19 @@ module CompareRunsSeleniumHelper
   		option = find(:xpath, option_xpath).text
   		select(option, :from => id)
 	end
+	
+	def table(id)
+		find_by_id(id)
+	end
+
+	def classnames_listed_for_table(tablename)
+		all(:xpath , "//table[@id='#{tablename}']/tbody/tr/td").map{|t| t.native.text}
+	end
+
+	def tests_failing_as(number_of_tests)
+		"Number of failing tests: #{number_of_tests}"
+	end
+
 
 	def perform_default_selections
 		select "COMPARE_RUNS_PROJECT" , from: "project_select"
@@ -55,13 +77,42 @@ describe "Compare Runs " , js:true do
 		perform_common_setup
 		create_new_project_and_subproject		
 		create_test_suites
-		open_compare_runs_page
-		perform_default_selections
 	end
 
 	after(:each){clean_up_data} 	
 
-	it "shows the project on the page" do		
-		sleep 30
+	context "when there are no failures" do 
+		before do 
+			open_compare_runs_page_and_perform_default_selection			
+		end
+
+		it "shows errors and error counts correctly" do
+			table("result_table_common").should have_content(0.tests_failing)
+			table("result_table_both").should have_content(0.tests_failing)
+			table("result_table_date1").should have_content(0.tests_failing)
+			table("result_table_date2").should have_content(0.tests_failing)			
+			classnames_listed_for_table("result_table_common").should be_empty
+			classnames_listed_for_table("result_table_both").should be_empty
+			classnames_listed_for_table("result_table_date1").should be_empty
+			classnames_listed_for_table("result_table_date2").should be_empty
+		end
 	end
+
+	context "when there are failures only for day 1" do 
+
+		before  do
+			create_class_errors(@suite1,class_error_hash_from("001","002","003","004"))
+			@expected_day_1_failures = class_name_arr_from "001","002","003","004"
+			open_compare_runs_page_and_perform_default_selection
+		end
+
+		it "shows errors and error counts correctly" do
+			sleep 30
+		end
+
+	end
+
+
+
+
 end
