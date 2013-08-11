56c841f903136cec14d3c4cc1ea9fe5f1e4a930e
Shrikant Khadilkar <sk176h@gmail.com>
2013-08-11 14:37:21 -0600

    View Specs for Compare Runs 1. added view specs for compare runs 2. fixed table display logic in getCompareJson.html.erb 3. added exclude filter to spechelper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,13 @@ SimpleCov.start
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  
+
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+
+
+
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
@@ -61,6 +68,8 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+
+  config.filter_run_excluding :ignore => true
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
--- spec/views/compare_runs/index.html.erb_spec.rb
+++ spec/views/compare_runs/index.html.erb_spec.rb
@@ -2,13 +2,24 @@ require 'spec_helper'
 require 'rspec'
 require File.expand_path("spec/helpers/comparesunsspechelper.rb")
 
+module CompareRunsCustomMatchers
+    RSpec::Matchers.define :contain_all do |expected|
+    	match do |actual|
+    		(actual.length == expected.length ) and (actual.to_set == expected.to_set)
+    	end
+    end
+
+    RSpec::Matchers.define :contain_none_of do |expected|
+        match do |actual|
+            expected & actual == []
+        end
+    end
+end
 
 class Fixnum
-
 	def tests_failing
 		"Number of failing tests: #{self}"
 	end
-
 end
 
 module CompareRunsSeleniumHelper
@@ -29,13 +40,8 @@ module CompareRunsSeleniumHelper
 	end
 
 	def classnames_listed_for_table(tablename)
-		all(:xpath , "//table[@id='#{tablename}']/tbody/tr/td").map{|t| t.native.text}
+		all(:xpath , "//table[@id='#{tablename}']/tbody/tr/td").map{|t| t.native.text} - [""]
 	end
-
-	def tests_failing_as(number_of_tests)
-		"Number of failing tests: #{number_of_tests}"
-	end
-
 
 	def perform_default_selections
 		select "COMPARE_RUNS_PROJECT" , from: "project_select"
@@ -71,6 +77,7 @@ describe "Compare Runs " , js:true do
 	self.use_transactional_fixtures = false	
 	include CompareRunsSpecHelper
 	include CompareRunsSeleniumHelper
+	include CompareRunsCustomMatchers
 	
 	before(:each) do
 		clean_up_data
@@ -79,9 +86,11 @@ describe "Compare Runs " , js:true do
 		create_test_suites
 	end
 
-	after(:each){clean_up_data} 	
+	after(:each){clean_up_data} 
+	
 
-	context "when there are no failures" do 
+	context "when there are no failures"  do 
+		
 		before do 
 			open_compare_runs_page_and_perform_default_selection			
 		end
@@ -98,7 +107,7 @@ describe "Compare Runs " , js:true do
 		end
 	end
 
-	context "when there are failures only for day 1" do 
+	context "when there are failures only for day 1"  do 
 
 		before  do
 			create_class_errors(@suite1,class_error_hash_from("001","002","003","004"))
@@ -107,12 +116,141 @@ describe "Compare Runs " , js:true do
 		end
 
 		it "shows errors and error counts correctly" do
-			sleep 30
+			table("result_table_common").should have_content(0.tests_failing)
+			table("result_table_both").should have_content(4.tests_failing)
+			table("result_table_date1").should have_content(4.tests_failing)
+			table("result_table_date2").should have_content(0.tests_failing)
+			classnames_listed_for_table("result_table_common").should be_empty
+			classnames_listed_for_table("result_table_both").should contain_all @expected_day_1_failures
+			classnames_listed_for_table("result_table_date1").should contain_all @expected_day_1_failures
+			classnames_listed_for_table("result_table_date2").should be_empty
 		end
 
 	end
 
+	context "when there are failures only for day 2"  do
+		before  do
+			create_class_errors(@suite2,class_error_hash_from("001","002","003","004"))
+			@expected_day_2_failures = class_name_arr_from "001","002","003","004"
+			open_compare_runs_page_and_perform_default_selection
+		end
 
+		it "shows errors and error counts correctly" do
+			table("result_table_common").should have_content(0.tests_failing)
+			table("result_table_both").should have_content(4.tests_failing)
+			table("result_table_date2").should have_content(4.tests_failing)
+			table("result_table_date1").should have_content(0.tests_failing)
+			classnames_listed_for_table("result_table_common").should be_empty
+			classnames_listed_for_table("result_table_both").should contain_all @expected_day_2_failures
+			classnames_listed_for_table("result_table_date1").should be_empty
+			classnames_listed_for_table("result_table_date2").should contain_all @expected_day_2_failures
+		end
+	end
 
+	context "when the failures for both days are exactly the same" do 		
+		before do
+			create_class_errors(@suite1,class_error_hash_from("001","002","003","004","005"))
+			create_class_errors(@suite2,class_error_hash_from("001","002","003","004","005"))
+			@expected_failures = class_name_arr_from "001","002","003","004","005"
+			open_compare_runs_page_and_perform_default_selection
+		end
 
+		it "shows errors and error counts correctly" do 
+			table("result_table_common").should have_content(5.tests_failing)
+			table("result_table_both").should have_content(5.tests_failing)
+			table("result_table_date2").should have_content(5.tests_failing)
+			table("result_table_date1").should have_content(5.tests_failing)
+			classnames_listed_for_table("result_table_common").should contain_all @expected_failures
+			classnames_listed_for_table("result_table_both").should contain_all @expected_failures
+			classnames_listed_for_table("result_table_date1").should contain_all @expected_failures
+			classnames_listed_for_table("result_table_date2").should contain_all @expected_failures
+		end
+	end
+
+	context "when the failures for both days are mutually exclusive" do 		
+		before do
+			create_class_errors(@suite1,class_error_hash_from("001","002","003"))
+			create_class_errors(@suite2,class_error_hash_from("004","005","006","007"))
+			@expected_day_1_failures = class_name_arr_from "001","002","003" 
+			@expected_day_2_failures = class_name_arr_from "004","005","006","007"
+			@expected_total_failures =  @expected_day_1_failures + @expected_day_2_failures
+			open_compare_runs_page_and_perform_default_selection
+		end
+
+		it "shows errors and error counts correctly" do 
+			table("result_table_common").should have_content(0.tests_failing)
+			table("result_table_both").should have_content(7.tests_failing)			
+			table("result_table_date1").should have_content(3.tests_failing)
+			table("result_table_date2").should have_content(4.tests_failing)
+			classnames_listed_for_table("result_table_common").should be_empty
+			classnames_listed_for_table("result_table_both").should contain_all @expected_total_failures
+			classnames_listed_for_table("result_table_date1").should contain_all @expected_day_1_failures 
+			classnames_listed_for_table("result_table_date2").should contain_all @expected_day_2_failures
+		end
+	end
+
+	context "when there are some common failures" do
+		before do
+			create_class_errors(@suite1,class_error_hash_from("001","002","003"))
+			create_class_errors(@suite2,class_error_hash_from("002","003","004"))
+			@expected_day_1_failures = class_name_arr_from "001","002","003" 
+			@expected_day_2_failures = class_name_arr_from "002","003","004"
+			@expected_common_failures = class_name_arr_from "002","003"
+			@expected_total_failures =  class_name_arr_from "001","002","003","004"
+			open_compare_runs_page_and_perform_default_selection
+		end
+
+		it "shows errors and error counts correctly" do 
+			table("result_table_common").should have_content(2.tests_failing)
+			table("result_table_both").should have_content(4.tests_failing)			
+			table("result_table_date1").should have_content(3.tests_failing)
+			table("result_table_date2").should have_content(3.tests_failing)
+			classnames_listed_for_table("result_table_common").should contain_all @expected_common_failures
+			classnames_listed_for_table("result_table_both").should contain_all @expected_total_failures
+			classnames_listed_for_table("result_table_date1").should contain_all @expected_day_1_failures 
+			classnames_listed_for_table("result_table_date2").should contain_all @expected_day_2_failures
+		end
+	end
+
+	context "when failures in day 1 are a subset of failures in day 2" do 
+		before do
+			create_class_errors(@suite1,class_error_hash_from("001","002","003"))
+			create_class_errors(@suite2,class_error_hash_from("001","002","003","004","005"))
+			@expected_day_1_failures = class_name_arr_from "001","002","003"
+			@expected_day_2_failures = class_name_arr_from "001","002","003","004","005"
+			open_compare_runs_page_and_perform_default_selection
+		end
+
+		it "shows errors and error counts correctly" do 
+			table("result_table_common").should have_content(3.tests_failing)
+			table("result_table_both").should have_content(5.tests_failing)			
+			table("result_table_date1").should have_content(3.tests_failing)
+			table("result_table_date2").should have_content(5.tests_failing)
+			classnames_listed_for_table("result_table_common").should contain_all @expected_day_1_failures
+			classnames_listed_for_table("result_table_both").should contain_all @expected_day_2_failures
+			classnames_listed_for_table("result_table_date1").should contain_all @expected_day_1_failures 
+			classnames_listed_for_table("result_table_date2").should contain_all @expected_day_2_failures
+		end
+	end
+
+	context "when failures in day 2 are a subset of failures in day 1" do 
+		before do
+			create_class_errors(@suite1,class_error_hash_from("001","002","003","004","005"))
+			create_class_errors(@suite2,class_error_hash_from("001","002","003"))
+			@expected_day_1_failures = class_name_arr_from "001","002","003","004","005"
+			@expected_day_2_failures = class_name_arr_from "001","002","003"
+			open_compare_runs_page_and_perform_default_selection
+		end
+
+		it "shows errors and error counts correctly" do 
+			table("result_table_common").should have_content(3.tests_failing)
+			table("result_table_both").should have_content(5.tests_failing)			
+			table("result_table_date1").should have_content(5.tests_failing)
+			table("result_table_date2").should have_content(3.tests_failing)
+			classnames_listed_for_table("result_table_common").should contain_all @expected_day_2_failures
+			classnames_listed_for_table("result_table_both").should contain_all @expected_day_1_failures
+			classnames_listed_for_table("result_table_date1").should contain_all @expected_day_1_failures 
+			classnames_listed_for_table("result_table_date2").should contain_all @expected_day_2_failures
+		end
+	end
 end
