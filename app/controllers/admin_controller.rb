284cfedeb447b43404b4fd4027f93e064f24de34
aasawaree <ritu.21090@gmail.com>
2013-08-29 12:18:36 +0530

    aasawaree :  code for external dashboard (partial)

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,4 +3,13 @@ class AdminController < ApplicationController
     projects=Project.all
     @json = Admin.get_result_json(projects)
   end
+
+  def add
+    #Code for adding urls to database here
+  end
+
+  def delete
+    #Delete the entry from db
+  end
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+    #Get data from the external dashboard table
+    @project = Hash.new
+    @project["Jira"] = "http://www.w3schools.com"
+    @project["Jenkins"] = "http://www.w3schools.com"
+    @project["Go"] = "http://www.w3schools.com"
+    @project = @project.to_json
   end
 end
