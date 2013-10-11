37edf6220f0328159454c5cae4c42a9c1b1f874a
aasawaree <ritu.21090@gmail.com>
2013-10-11 10:43:58 +0530

    aasawaree: validation for unqiue name in test_category and test_sub_category in test_category_mapping

--- app/models/test_category_mapping.rb
+++ app/models/test_category_mapping.rb
@@ -1,5 +1,9 @@
 class TestCategoryMapping < ActiveRecord::Base
   attr_accessible :test_category, :test_sub_category
+  validates_uniqueness_of :test_category
+  validates_uniqueness_of :test_sub_category
+  validates_presence_of :test_sub_category
+  validates_presence_of :test_category
 
   def get_sub_category(test_category)
     TestCategoryMapping.where(:test_category => test_category).select(:test_sub_category)
