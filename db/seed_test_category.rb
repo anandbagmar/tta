dbae61cd68a59a292e7825b635129082cde36c97
aasawaree <ritu.21090@gmail.com>
2013-10-11 09:23:46 +0530

    aasawaree : added new test sub category from rake , if test sub category is not defined

--- db/seed_test_category.rb
+++ db/seed_test_category.rb
@@ -2,7 +2,7 @@ TESTCATEGORYMAPPINGS = YAML.load(File.open("#{Rails.root}/config/test_category_m
 TESTCATEGORYMAPPINGS.each do |testCategoryMapping|
   test_category = testCategoryMapping.first
   TESTCATEGORYMAPPINGS[test_category].each do |test_sub_category|
-    TestCategoryMapping.create(:test_category => test_category, :test_sub_category => test_sub_category)
+    TestCategoryMapping.create(:test_category => test_category.upcase, :test_sub_category => test_sub_category.upcase)
   end
 end
 
