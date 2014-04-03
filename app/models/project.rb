5f9a82269b1b641254fed39e173ad53c9dba6a60
Sandeep Mankar <smankar@thoughtworks.com>
2014-04-03 07:25:50 +0000

    [Sandeep] - Upcase project.name in model and not in controller

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,17 @@
 class Project < ActiveRecord::Base
   has_many :sub_projects
   attr_accessible :authorization_level, :name
-  validates_uniqueness_of :name
+  before_validation :uppercase_name
+  validates_uniqueness_of :name, :case_sensitive => false
   validates_presence_of :name
 
   scope :get_all_projects, select("id, name");
 
   def add_sub_project(params)
-    sub_projects.find_or_create_by_name((params[:sub_project_name]).split.join(" ").upcase)
+    sub_projects.find_or_create_by_name((params[:sub_project_name]).split.join(" "))
+  end
+
+  def uppercase_name
+      self.name.upcase! if !self.name.nil?
   end
 end
