baae5dc75c7d36c46fe923bc2ce57f7eb7a628ef
Anand Bagmar <abagmar@gmail.com>
2017-05-12 13:46:31 +0530

    do not restart mysql when starting tea

--- startTTA.sh
+++ startTTA.sh
@@ -13,11 +13,6 @@ sleep 2
 echo "--> MySQL server status - "
 mysql.server status
 
-echo "--> Restart MySQL server"
-mysql.server restart
-
-sleep 3
-
 echo "--> Start TTA on IP $1:3000 in '$RAILS_ENV' mode"
 bundle exec rails s -b $1 &
 
